fea6700ebb75075d8a01d30da7d1784fe8647b68
kagemomiji <kagemomiji.dev@gmail.com>
2022-05-01 12:12:51 +0000

    [update] move extract meals to class constructor

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import request from 'request';
-import cheerio from 'cheerio';
 import Meals from './model/Meals';
-import Meal from './model/Meal';
 
 const url = process.argv[2];
 console.log(url);
-
-const RECIPE_SPEC_TAG = 'h3';
 
 request(url, (e: any, _response, body: string) => {
     if (e) {
         console.error(e)
     }
     try {
-        const meals: Meals = extractRecipeLinks(body);
-        console.log("メイン");
-        
+        const meals: Meals = new Meals(body);
+       
         meals.main.forEach((v , _i) => {
             console.log(v);
         });
@@ -23,26 +18,8 @@ request(url, (e: any, _response, body: string) => {
         meals.sub.forEach((v , i) => {
             console.log(v);
         });
+        console.log(meals.html());
      } catch (e) {
          console.error(e)
      }
 })
-
-
-const extractRecipeLinks = (body: string) => {
-    const $ = cheerio.load(body);
-    let isSubMeal: boolean = false;
-    let mainMeals: Meal[] = [];
-    let subMeals: Meal[] = [];
-    $('#page_recipe > div > div').children().each( (_i: number, element: cheerio.Element) => {
-        if (element.type === "tag" &&  element.name === RECIPE_SPEC_TAG && $(element).text() === "副菜"){
-            isSubMeal = true;
-        }
-        if (element.type === "tag" && element.name !== RECIPE_SPEC_TAG){
-            let meal = new Meal($(element).text(), $(element).find('a').attr('href'))
-            if (isSubMeal) subMeals.push(meal);
-            else mainMeals.push(meal);
-        }
-    });
-    return new Meals(mainMeals, subMeals);
-}
--- src/model/Meals.ts
+++ src/model/Meals.ts
@@ -1,11 +1,26 @@
+import cheerio from 'cheerio';
 import Meal from "./Meal";
 
+const RECIPE_SPEC_TAG = 'h3';
+
 export default class Meals {
-    private _main: Meal[]
-    private _sub: Meal[]
-    constructor(main: Meal[], sub: Meal[]) {
-        this._main = main;
-        this._sub = sub;
+    private _main: Meal[] = []
+    private _sub: Meal[] = []
+    private _content: cheerio.Cheerio
+    constructor(body: string) {
+        const $ = cheerio.load(body);
+        let isSubMeal: boolean = false;
+        this._content = $('section').has('#step1').children('#page_recipe').children().children();
+        this._content.children().each( (_i: number, element: cheerio.Element) => {
+            if (element.type === "tag" &&  element.name === RECIPE_SPEC_TAG && $(element).text() === "副菜"){
+                isSubMeal = true;
+            }
+            if (element.type === "tag" && element.name !== RECIPE_SPEC_TAG){
+                let meal = new Meal($(element).text(), $(element).find('a').attr('href'))
+                if (isSubMeal) this._sub.push(meal);
+                else this._main.push(meal);
+            }
+        });
     }
 
     public get main() {
@@ -14,5 +29,16 @@ export default class Meals {
 
     public get sub() {
         return this._sub;
+    }
+
+    public all(): Meal[]{
+        let all: Meal[] = [];
+        this._main.forEach((meal: Meal, _i) => all.push(meal));
+        this._sub.forEach((meal: Meal, _i) => all.push(meal));
+        return all;
+    }
+
+    public html() : string | null {
+        return this._content.parents('#page_recipe').html();
     }
 }
