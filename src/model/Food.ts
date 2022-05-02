0ed9014fb6d78a0588f0af01630eed426dc0244a
kagemomiji <kagemomiji.dev@gmail.com>
2022-05-02 07:13:38 +0000

    [update] removed 約 from amount

--- src/model/Food.ts
+++ src/model/Food.ts
@@ -6,7 +6,7 @@ export default class Food {
         this._name = name.replace(/(◎|○|◯)/,"");
         this._amount = amount.replace(/[Ａ-Ｚａ-ｚ０-９／]/g, (s) => {
             return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-        });
+        }).replace("約","");
     }
 
     get name(){
