b912e041f1c62bb6d72e6dcdbb8b24b6f5fd419f
kagemomiji <kagemomiji.dev@gmail.com>
2022-05-06 04:40:54 +0000

    [update] added `.` to zenkaku hankaku converter

--- src/model/Food.ts
+++ src/model/Food.ts
@@ -9,7 +9,7 @@ export default class Food {
         
         this._name = name.replace(/(◎|◯|●)/,"").replace(/（$/,"");
         this._alias = name.replace(this._name, "").replace(/（$/,"");
-        this._amount = amount.replace(/[Ａ-Ｚａ-ｚ０-９／]/g, (s) => {
+        this._amount = amount.replace(/[Ａ-Ｚａ-ｚ０-９／．]/g, (s) => {
             return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
         }).replace("約","");
         if(meal !== undefined){
