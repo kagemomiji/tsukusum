6d5582838bbbf9f3bd7074bf684c23a74b967e77
kagemomiji <kagemomiji.dev@gmail.com>
2022-05-08 12:21:45 +0000

    [clean] removed output files

--- frontend/dist/common/const/PlantUml.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLANT_UML = void 0;
-exports.PLANT_UML = {
-    START_UML: '@startuml',
-    END_UML: '@enduml',
-};
